5a911cf55f99b86146abf0fe6b706859a9feb2f7
AndreOliveiraMendes <ao_mendes@hotmail.com>
2025-05-06 18:34:35 +0000

    improved flame tier computation logic

--- pages/flame.py
+++ pages/flame.py
@@ -1,7 +1,18 @@
-from itertools import product, combinations
+from itertools import product
 
 def get_tiers(stats, ps, ms):
-    stats_tiers, tiers = {1: [], 2: [], 3: [], 4: []}, []
+    tiers = []
+    if ms == 0:
+        tier = []
+        for i in range(1, 5):
+            if stats[i] % ps != 0:
+                return tiers
+            tier.append(stats[i]//ps)
+        tier = tier + [0]*6
+        tiers.append(tier)
+        return tiers
+
+    stats_tiers = {1: [], 2: [], 3: [], 4: []}
     for i in range(1, 5):
         for t in range(8):
             if stats[i] - ps * t >= 0 and (stats[i] - ps * t) % ms == 0:
@@ -9,24 +20,46 @@ def get_tiers(stats, ps, ms):
     if any(len(v) == 0 for v in stats_tiers.values()):
         return tiers
 
+    #t1*, t2*, t3*, t4*, t12, t13, t14, t23, t24*, t34*
     for combo in product(*[stats_tiers[i] for i in range(1, 5)]):
+        t1, t2, t3, t4 = combo
+        s1, s2, s3, s4 = stats.values()
         mixed_stats_total_tier = {
             i: (stats[i] - ps * combo[i - 1]) // ms for i in range(1, 5)
         }
         possibilities = [
             list(range(min(7, mixed_stats_total_tier[i], mixed_stats_total_tier[j]) + 1))
-            for i, j in combinations(range(1, 5), 2)
+            for i, j in [(2, 4), (3, 4)]
         ]
-        for possible_tiers in product(*possibilities):
-            s = {}
-            s[1] = combo[0] * ps + sum(possible_tiers[i] for i in [0, 1, 2]) * ms
-            s[2] = combo[1] * ps + sum(possible_tiers[i] for i in [0, 3, 5]) * ms
-            s[3] = combo[2] * ps + sum(possible_tiers[i] for i in [1, 3, 5]) * ms
-            s[4] = combo[3] * ps + sum(possible_tiers[i] for i in [2, 4, 5]) * ms
-            if all(s[i] == stats[i] for i in range(1, 5)):
-                tier = list(combo) + list(possible_tiers)
-                tiers.append(tier)
+        for free_mixed_tiers in product(*possibilities):
+            t24, t34 = free_mixed_tiers
+            t12 = ps*(t3+t4-t1-t2)+(s1+s2-s3-s4)
+            t13 = ps*(t2+t4-t1-t3)+(s1+s3-s2-s4)
+            t14 = -ps*t4+s4
+            t23 = ps*(t1-t2-t3-t4)+(s2+s3+s4-s1)
+            mod = 2*ms
+            if t12%mod != 0 or t13%mod != 0 or t14%ms != 0 or t23%mod != 0: #check for possible non integer tiers
+                continue
+            t12 = t12//mod + ms*t34
+            t13 = t13//mod + ms*t24
+            t14 = t14//ms - ms*(t24+t34)
+            t23 = t23//mod - ms*(t24+t34)
+            possible_tiers = [t12, t13, t14, t23, t24, t34]
+            if any(t > 7 for t in possible_tiers):
+                continue
+            tier = list(combo) + possible_tiers
+            tiers.append(tier)
     return tiers
 
 def count_groups_used(tier):
     return sum(1 for v in tier if v > 0)
+
+if __name__ == "__main__":
+    stats = {1:24, 2:24, 3:0, 4:0}
+    ps = 12
+    ms = 7
+    tiers = get_tiers(stats, ps, ms)
+    print(tiers)
+    ms = 0
+    tiers = get_tiers(stats, ps, ms)
+    print(tiers)
