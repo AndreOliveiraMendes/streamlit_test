37ff89302412c4b76cb2972df16f6f4f9bdbafcc
AndreOliveiraMendes <ao_mendes@hotmail.com>
2025-05-06 19:51:58 +0000

    fixed flame logic

--- pages/flame.py
+++ pages/flame.py
@@ -1,5 +1,5 @@
 import streamlit as st
-from itertools import product
+from itertools import product, combinations
 
 st.title("Small Helper Calculator for MapleStory Bonus Stat")
 
@@ -25,35 +25,23 @@ def get_tiers(stats, ps, ms):
     if any(len(v) == 0 for v in stats_tiers.values()):
         return tiers
 
-    #t1*, t2*, t3*, t4*, t12, t13, t14, t23, t24*, t34*
     for combo in product(*[stats_tiers[i] for i in range(1, 5)]):
-        t1, t2, t3, t4 = combo
-        s1, s2, s3, s4 = stats.values()
         mixed_stats_total_tier = {
             i: (stats[i] - ps * combo[i - 1]) // ms for i in range(1, 5)
         }
         possibilities = [
             list(range(min(7, mixed_stats_total_tier[i], mixed_stats_total_tier[j]) + 1))
-            for i, j in [(2, 4), (3, 4)]
+            for i, j in combinations(range(1, 5), 2)
         ]
-        for free_mixed_tiers in product(*possibilities):
-            t24, t34 = free_mixed_tiers
-            t12 = ps*(t3+t4-t1-t2)+(s1+s2-s3-s4)
-            t13 = ps*(t2+t4-t1-t3)+(s1+s3-s2-s4)
-            t14 = -ps*t4+s4
-            t23 = ps*(t1-t2-t3-t4)+(s2+s3+s4-s1)
-            mod = 2*ms
-            if t12%mod != 0 or t13%mod != 0 or t14%ms != 0 or t23%mod != 0: #check for possible non integer tiers
-                continue
-            t12 = t12//mod + ms*t34
-            t13 = t13//mod + ms*t24
-            t14 = t14//ms - ms*(t24+t34)
-            t23 = t23//mod - ms*(t24+t34)
-            possible_tiers = [t12, t13, t14, t23, t24, t34]
-            if any(t > 7 for t in possible_tiers):
-                continue
-            tier = list(combo) + possible_tiers
-            tiers.append(tier)
+        for possible_tiers in product(*possibilities):
+            s = {}
+            s[1] = combo[0] * ps + sum(possible_tiers[i] for i in [0, 1, 2]) * ms
+            s[2] = combo[1] * ps + sum(possible_tiers[i] for i in [0, 3, 4]) * ms
+            s[3] = combo[2] * ps + sum(possible_tiers[i] for i in [1, 3, 5]) * ms
+            s[4] = combo[3] * ps + sum(possible_tiers[i] for i in [2, 4, 5]) * ms
+            if all(s[i] == stats[i] for i in range(1, 5)):
+                tier = list(combo) + list(possible_tiers)
+                tiers.append(tier)
     return tiers
 
 def count_groups_used(tier):
