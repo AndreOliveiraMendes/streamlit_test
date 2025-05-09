084ead721e8f4d0f22614f8f2c93af01e63af61b
AndreOliveiraMendes <ao_mendes@hotmail.com>
2025-05-09 20:23:32 +0000

    added a writer for bonust stat n2 (still in development)

---
+++ pages/system.py
@@ -0,0 +1,138 @@
+import streamlit as st, textwrap
+from pages.utils.system_util import generate_extended_matrix, generate_auxiliar_matrix, decode_step, scale_by_a_factor
+from math import gcd
+from itertools import combinations
+
+def write_extended_matrix_markdown(matrix, extended_matrix_introduction = None):
+    header_size = len(matrix[0])
+    header = ''.join(['c']*(header_size - 1) + [':c'])
+    content = '\\\\\n'.join(['&'.join(map(str, row)) for row in matrix])
+    markdown = textwrap.dedent(f"""
+    $$
+    \\left[\\begin{{array}}{{{header}}}
+    {content}
+    \\end{{array}}\\right]
+    $$
+    """).replace("\t", "").replace("    ", "").replace("&s", "&s_")
+    if extended_matrix_introduction:
+        markdown = f"{extended_matrix_introduction}\n{markdown}"
+    st.code(markdown, language="latex")
+
+def write_sage_steps(matrix, num_stats, sage_code_introduction = None, ignore_comments = True):
+    variables = ['p', 'm'] + [f's{i}' for i in range(1, num_stats + 1)]
+    sage_variables_definition = f"var('{' '.join(variables)}')"
+    aux = ',\n    '.join([f"[{', '.join(row)}]" for row in matrix])
+    sage_matrix_definition = f"M = matrix([\n    {aux}\n])"
+    aux_matrix = generate_auxiliar_matrix(num_stats )
+    aux_steps = []
+    matrix_width = len(aux_matrix[0])
+    if not ignore_comments:
+        aux_steps.append(("comment", "fist step: upper triangularization"))
+    for i in range(num_stats):
+        if aux_matrix[i][i] == 0:
+            for j in range(i, num_stats):
+                if aux_matrix[j][i] != 0:
+                    aux_matrix[i], aux_matrix[j] = aux_matrix[j], aux_matrix[i]
+                    aux_steps.append(("swap", i, j))
+        pivot = aux_matrix[i][i]
+        if pivot == 0:
+            raise NotImplementedError("the function can't handle this case yet")
+        for j in range(i + 1, num_stats):
+            if (ref := aux_matrix[j][i]) == 0:
+                continue
+            if ref%pivot != 0:
+                ref, pivot = scale_by_a_factor(aux_matrix, matrix_width, i, j, pivot, ref, aux_steps)
+            factor = ref // pivot
+            for k in range(matrix_width):
+                aux_matrix[j][k] -= factor * aux_matrix[i][k]
+            aux_steps.append(("subtract", i, j, factor))
+    if not ignore_comments:
+        aux_steps.append(("comment", "second step: back substitution"))
+    for i in range(num_stats-1, -1, -1):
+        pivot = aux_matrix[i][i]
+        for j in range(i - 1, -1, -1):
+            if(ref := aux_matrix[j][i]) == 0:
+                continue
+            if ref%pivot != 0:
+                ref, pivot = scale_by_a_factor(aux_matrix, matrix_width, i, j, pivot, ref, aux_steps)
+            factor = aux_matrix[j][i] // aux_matrix[i][i]
+            for k in range(matrix_width):
+                aux_matrix[j][k] -= factor * aux_matrix[i][k]
+            aux_steps.append(("subtract", i, j, factor))
+    if not ignore_comments:
+        aux_steps.append(("comment", "third step: normalization"))
+    for i in range(num_stats):
+        pivot = aux_matrix[i][i]
+        factor = pivot
+        for k in range(matrix_width):
+            negative = False
+            p = aux_matrix[i][k]
+            if (p > 0 and factor < 0) or (p < 0 and factor > 0):
+                negative = True
+            p, q = abs(p), abs(factor)
+            d = gcd(p, q)
+            p //= d
+            q //= d
+            aux_matrix[i][k] = ("-" if negative else '') + (f"{p}" if q == 1 else f"{p}/{q}")
+        if factor != 1:
+            aux_steps.append(("divide", i, factor))
+    sage_step = [decode_step(step) for step in aux_steps]
+    sage_step = '\n'.join(sage_step)
+    final_sage_code = ""
+    if sage_code_introduction is not None:
+        final_sage_code += f"{sage_code_introduction}\n\n"
+    final_sage_code += f"```sage\n{sage_variables_definition}\n{sage_matrix_definition}\n{sage_step}\nM\n```"
+    st.code(final_sage_code, language="markdown")
+    return aux_matrix
+
+def write_system_solution(matrix, num_stats, solution_introduction = None):
+    total = len(matrix[0])
+    head = [f"pt_{{{i + 1}}}" for i in range(num_stats)] \
+        + [f"mt_{{{i + 1},{j + 1}}}" for i, j in combinations(range(num_stats), 2)] \
+        + [f"s_{{{i + 1}}}" for i in range(num_stats)]
+    latext_code = ""
+    if solution_introduction is not None:
+        latext_code += f"{solution_introduction}\n\n"
+    latext_code += "$$\begin{cases}"
+    st.write(matrix)
+    return
+    for i in range(num_stats):
+        equation = []
+        for j in range(num_stats, total):
+            if matrix[i][j] == "0":
+                continue
+            if '-' in matrix[i][j]:
+                equation.append(f"+{matrix[i][j].replace('-', '')}*{head[j]}")
+            else:
+                equation.append(f"-{matrix[i][j]}*{head[j]}")
+        equation
+
+
+st.title("System")
+
+with st.expander("Sobre"):
+    st.write(
+        r"""
+        Este sistema gera o código Markdown para uma generalização de um sistema com *n* atributos (stats).
+
+        Ele constrói a matriz estendida de um sistema de equações lineares, dado por:
+
+        $$\displaystyle s_i = pt_i + m \sum_{j \neq i} t_{i,j}$$
+
+        As equações podem ser resolvidas utilizando ferramentas como o [SageMathCell](https://sagecell.sagemath.org).
+        """
+    )
+
+num_stats = st.number_input("Number of Stats", min_value=2, max_value=10, step=1, value=5, help="Número de atributos (stats) no sistema.")
+extended_matrix_introduction = st.text_input("Extended Matrix Introduction", value="the extended matrix is given by:", help="texto de apresentação da matriz estendida.")
+extended_sage_code_introduction = st.text_input("Sage Code Introduction", value="the sage code is given by:", help="texto de apresentação do codigo sage.")
+if st.button("gerar codigo"):
+    extended_matrix = generate_extended_matrix(num_stats)
+    with st.expander("Extended Matrix"):
+        write_extended_matrix_markdown(extended_matrix, extended_matrix_introduction)
+    reduced_matrix = None
+    with st.expander("Sage Code"):
+        reduced_matrix = write_sage_steps(extended_matrix, num_stats, extended_sage_code_introduction, False)
+    with st.expander("System Solution"):
+        write_system_solution(reduced_matrix, num_stats)
+    
---
+++ pages/utils/system_util.py
@@ -0,0 +1,78 @@
+from itertools import combinations
+from math import lcm
+def generate_extended_matrix(num_stats):
+    matrix = []
+    num_mixed = num_stats * (num_stats - 1)//2
+    for i in range(1, num_stats + 1):
+        row = []
+        mixed_index = combinations(range(1, num_stats + 1), 2)
+        for j, combo in zip(range(1, num_mixed + 1), mixed_index):
+            j, k = combo
+            if i == j or i == k:
+                row.append("m")
+            else:
+                row.append("0")
+        for j in range(1, num_stats + 1):
+            if i == j:
+                row.append("p")
+            else:
+                row.append("0")
+        row.append("s" + str(i))
+        matrix.append(row)
+    return matrix
+
+def generate_auxiliar_matrix(num_stats):
+    matrix = []
+    num_mixed = num_stats * (num_stats - 1)//2
+    for i in range(1, num_stats + 1):
+        row = []
+        mixed_index = combinations(range(1, num_stats + 1), 2)
+        for j, combo in zip(range(1, num_mixed + 1), mixed_index):
+            j, k = combo
+            if i == j or i == k:
+                row.append(1)
+            else:
+                row.append(0)
+        for j in range(1, num_stats + 1):
+            if i == j:
+                row.append(1)
+            else:
+                row.append(0)
+        for j in range(1, num_stats + 1):
+            if i == j:
+                row.append(1)
+            else:
+                row.append(0)
+        matrix.append(row)
+    return matrix
+
+def decode_step(step):
+    if step[0] == "swap":
+        i, j = step[1], step[2]
+        return "M[{i}], M[{j}] = M.row({j}), M.row({j})"
+    elif step[0] == "subtract":
+        i, j, factor = step[1], step[2], step[3]
+        return f"M[{j}] = M.row({j}){'-' if factor > 0 else '+'}{str(abs(factor)) + '*' if abs(factor) != 1 else ''}M.row({i})"
+    elif step[0] == "multiply":
+        i, factor = step[1], step[2]
+        return f"M[{i}] = {'+' if factor > 0 else '-'}{str(abs(factor)) + '*' if abs(factor) != 1 else ''}M.row({i})"
+    elif step[0] == "comment":
+        comment = step[1]
+        return f"# {comment}"
+    elif step[0] == "divide":
+        i, factor = step[1], step[2]
+
+        return f"M[{i}] = {'-' if factor < 0 else ''}M.row({i}){'/' + str(abs(factor)) if abs(factor) != 1 else ''}"
+    else:
+        raise NotImplementedError(f"Unknown step type: {step[0]}")
+    
+def scale_by_a_factor(matrix, matrix_width, i, j, pivot, ref, steps):
+    common_ground = lcm(ref, pivot)
+    scale_ref = common_ground // ref
+    scale_pivot = common_ground // pivot
+    steps.append(("multiply", i, scale_pivot))
+    steps.append(("multiply", j, scale_ref))
+    for k in range(matrix_width):
+        matrix[i][k] *= scale_pivot
+        matrix[j][k] *= scale_ref
+    return common_ground, common_ground
