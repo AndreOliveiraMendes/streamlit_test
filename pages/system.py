d9fde786036c4fb83b445393c93e4e591fe3cbda
AndreOliveiraMendes <ao_mendes@hotmail.com>
2025-05-10 03:09:43 +0000

    advanced into system solution logic.

--- pages/system.py
+++ pages/system.py
@@ -1,6 +1,6 @@
 import streamlit as st, textwrap
-from pages.utils.system_util import generate_extended_matrix, generate_auxiliar_matrix, decode_step, scale_by_a_factor
-from math import gcd
+from pages.utils.system_util import generate_extended_matrix, generate_auxiliar_matrix, decode_step, scale_by_a_factor, value_format
+from math import gcd, lcm
 from itertools import combinations
 
 def write_extended_matrix_markdown(matrix, extended_matrix_introduction = None):
@@ -85,28 +85,58 @@ def write_sage_steps(matrix, num_stats, sage_code_introduction = None, ignore_co
     st.code(final_sage_code, language="markdown")
     return aux_matrix
 
+def get_variable(value:str, var:str, remove_multiplier = False):
+    kind = "pure" if "p" in var else ("mixed" if "m" in var else "stats")
+    if remove_multiplier:
+        var = var.replace("p", "").replace("m", "")
+    parts = list(value.split("/"))
+    num, den = 0, 0
+    if len(parts) == 2:
+        num, den = map(int, parts)
+    else:
+        num, den = int(parts[0]), 1
+    
+    return [var, [num, den]], kind
+
+def sort_variable(var):
+    return (-1 if var[1][0] >= 0 else 1, var[0])
+
 def write_system_solution(matrix, num_stats, solution_introduction = None):
     total = len(matrix[0])
-    head = [f"pt_{{{i + 1}}}" for i in range(num_stats)] \
-        + [f"mt_{{{i + 1},{j + 1}}}" for i, j in combinations(range(num_stats), 2)] \
+    head = [f"mt_{{{i + 1},{j + 1}}}" for i, j in combinations(range(num_stats), 2)] \
+        + [f"pt_{{{i + 1}}}" for i in range(num_stats)] \
         + [f"s_{{{i + 1}}}" for i in range(num_stats)]
-    latext_code = ""
+    latex_code = ""
     if solution_introduction is not None:
-        latext_code += f"{solution_introduction}\n\n"
-    latext_code += "$$\begin{cases}"
-    st.write(matrix)
-    return
+        latex_code += f"{solution_introduction}\n\n"
+    latex_code += "$$\\begin{cases}\n"
     for i in range(num_stats):
-        equation = []
+        depedent, _ = get_variable(matrix[i][i], head[i])
+        equation = {"pure":[], "mixed":[], "stats":[]}
         for j in range(num_stats, total):
-            if matrix[i][j] == "0":
-                continue
-            if '-' in matrix[i][j]:
-                equation.append(f"+{matrix[i][j].replace('-', '')}*{head[j]}")
-            else:
-                equation.append(f"-{matrix[i][j]}*{head[j]}")
-        equation
-
+            free, kind = get_variable(matrix[i][j], head[j], True)
+            if kind in ["pure", "mixed"]:
+                free[1][0] *= -1
+            if free[1][0] != 0:
+                equation[kind].append(free)
+        latex_code += depedent[0] + "="
+        pure_common = lcm(*[val[1][1] for val in equation["pure"]])
+        if len(equation["pure"]) > 1:
+            pure_part_open, pure_part_close = "p(", ")+"
+            if pure_common > 1:
+                pure_part_open = f"\\frac{{p}}{{{pure_common}}}("
+            latex_code += pure_part_open
+            latex_code += ''.join(value_format(var[1][0]) + var[0] for var in sorted(equation["pure"], key=sort_variable))
+            latex_code += pure_part_close
+        elif (q := equation['pure'][0][1][1]) != 1:
+            latex_code += f"\\frac{{{value_format(equation['pure'][0][1][0])}p}}{{{q}}}"
+        else:
+            latex_code += f"{value_format(equation['pure'][0][0])}p"
+        mixed_common = lcm(*[val[1][1] for val in equation["mixed"]])
+        stats_common = lcm(*[val[1][1] for val in equation["stats"]])
+        latex_code += "\\\\\n"
+    latex_code += "$$"
+    st.code(latex_code, language="latex")
 
 st.title("System")
 
