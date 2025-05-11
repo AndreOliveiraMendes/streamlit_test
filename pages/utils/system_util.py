00e80e6bfb490f39a6fe70f077ac5a16fa5a1398
AndreOliveiraMendes <ao_mendes@hotmail.com>
2025-05-11 04:36:55 +0000

    updated system_util f-strings

--- pages/utils/system_util.py
+++ pages/utils/system_util.py
@@ -1,6 +1,17 @@
 from itertools import combinations
 from math import lcm
-def generate_extended_matrix(num_stats):
+def generate_extended_matrix(num_stats: int) -> list[list[str]]:
+    """
+    gera a matrix estendida do sistema de equações decorendo do problem "n-{1,2}"
+    
+    Parâmetros:
+
+    - num_stats (int): o numero de atributos do sistema
+
+    Retorna:
+
+    - list[list[str]]: uma matrix de n linhas e n + n*(n-1)/2 + 1 colunas que representa simbolicamente a matrix extendida
+    """
     matrix = []
     num_mixed = num_stats * (num_stats - 1)//2
     for i in range(1, num_stats + 1):
@@ -21,7 +32,18 @@ def generate_extended_matrix(num_stats):
         matrix.append(row)
     return matrix
 
-def generate_auxiliar_matrix(num_stats):
+def generate_auxiliar_matrix(num_stats: int) -> list[list[int]]:
+    """
+    gera a matrix auxiliar do sistema de equações decorendo do problem "n-{1,2}"
+    
+    Parâmetros:
+
+    - num_stats (int): o numero de atributos do sistema
+
+    Retorna:
+
+    - list[list[int]]: uma matrix de n linhas e n + n*(n-1)/2 + n colunas para auxilio da extração da logica de escalonamento do sistema
+    """
     matrix = []
     num_mixed = num_stats * (num_stats - 1)//2
     for i in range(1, num_stats + 1):
@@ -46,7 +68,19 @@ def generate_auxiliar_matrix(num_stats):
         matrix.append(row)
     return matrix
 
-def value_format(value, extra=""):
+def value_format(value: int | str, extra: str = "") -> str:
+    """
+    formata um valor inteiro/string para exibição nos passos do sage e em lugais em que havera multiplicações (salvo algumas exceções) 
+    
+    Parâmetros:
+
+    - value (int ou str): o valor a ser tratado
+    - extra (str) = "": um variavel extra para controle de exibição do sinal de multiplicação, se necessario, sendo nada exibido por padrão
+
+    Retorna:
+
+    - str: uma string formatada para uso
+    """
     result = ""
     if isinstance(value, int):
         result = ('+' if value > 0 else '-') + (str(abs(value)) + extra if abs(value) != 1 else '')
@@ -54,7 +88,23 @@ def value_format(value, extra=""):
         result = ('+' if not '-' in value else '-') + (value + extra if not value in ['1', '-1'] else '')
     return result.replace("--", "+")
 
-def decode_step(step):
+def decode_step(step: tuple) -> str:
+    """
+    decodifica os passos gerados pela matrix auxiliar em linguagem compativel com editores simbolicos como o sagemath, os passos implementados são:
+    - ("swap", i: int, j: int): operação de troca entre as linhas i e j
+    - ("subtract", i: int, j: int, fator: int): operação de subtração da linha j pela linha i multiplicada de um fator inteiro
+    - ("multiply", i: int, fator: int): operação de multiplifação da linha i por um fator
+    - ("comment", msg: str): insere uma linha de comentario
+    - ("divide", fator: int): analogo a "multiply", porem efetua a divisão
+    
+    Parâmetros:
+
+    - step (tuple): uma tupla indicando um passo, explicado acima
+
+    Retorna:
+
+    - str: uma string correspodente ao passo traduzido em linguagem compativel com editores simbolicos como o sagemath
+    """
     if step[0] == "swap":
         i, j = step[1], step[2]
         return "M[{i}], M[{j}] = M.row({j}), M.row({j})"
@@ -73,7 +123,24 @@ def decode_step(step):
     else:
         raise NotImplementedError(f"Unknown step type: {step[0]}")
 
-def scale_by_a_factor(matrix, i, j, steps, matrix_width = None, pivot = None, ref = None):
+def scale_by_a_factor(matrix: list[list[int]], i: int, j: int, steps: list[tuple], matrix_width: int | None = None, pivot: int | None = None, ref: int | None = None) -> tuple[int, int]:
+    """
+    efetua a sincronização da linha i com a linha j da matrix auxiliar a fim de evitar a divisão e eventuais problemas de precisão numerica
+    
+    Parâmetros:
+
+    - matrix (list[list[int]]): a matrix auxiliar
+    - i (int): o indice da primeira linha, 0-indexado
+    - j (int): o indice da segunda linha, 0-indexado
+    - steps (list[tuple]): uma lista a qual sera populada com os passos equivalente gerado nessa operação
+    - matrix_width (int ou None): um inteiro representando a largura da linha, sendo calculada com base na primeira linha caso não informado
+    - pivot (int ou None): o elemento pivo da linha i, sendo calculado como o elemento de indice i da linha i caso não informado
+    - ref (int ou None): o elemento de referencia da linha j, sendo calculado como o elemento de indice i da linha j caso não informado
+
+    Retorna:
+
+    - tuple[int, int]: uma tupla de elementos iguais (o minimo multiplo comun do pivo e do elemento de referencia), com substituir ambos o pivo e a referencia simultaneamente)
+    """
     matrix_width = len(matrix[i]) if matrix_width is None else matrix_width
     pivot = matrix[i][i] if pivot is None else pivot
     ref = matrix[j][i] if ref is None else ref
@@ -87,7 +154,23 @@ def scale_by_a_factor(matrix, i, j, steps, matrix_width = None, pivot = None, re
         matrix[j][k] *= scale_ref
     return common_ground, common_ground
 
-def apply_row_elimination(matrix, i, j, steps, matrix_width = None, pivot = None):
+def apply_row_elimination(matrix: list[list[int]], i: int, j: int, steps: list[tuple], matrix_width: int | None = None, pivot: int | None = None) -> None:
+    """
+    aplica o processo de eliminação da linha j pela linha i, funciona tanto na subida quando na descida
+    
+    Parâmetros:
+
+    - matrix (list[list[int]]): a matrix auxiliar
+    - i (int): o indice da primeira linha, 0-indexado
+    - j (int): o indice da segunda linha, 0-indexado
+    - steps (list[tuple]): uma lista a qual sera populada com os passos equivalente gerado nessa operação
+    - matrix_width (int ou None): um inteiro representando a largura da linha, sendo calculada com base na primeira linha caso não informado
+    - pivot (int ou None): o elemento pivo da linha i, sendo calculado como o elemento de indice i da linha i caso não informado
+
+    Retorna:
+
+    - None: não há retorno, apenas a população da lista steps com os passos equivalentes
+    """
     matrix_width = len(matrix[i]) if matrix_width is None else matrix_width
     pivot = matrix[i][i] if pivot is None else pivot
     if(ref := matrix[j][i]) == 0:
