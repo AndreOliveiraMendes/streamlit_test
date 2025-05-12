2b3e58c10bdf5d5d60b8e91df342bfd997d26525
AndreOliveiraMendes <ao_mendes@hotmail.com>
2025-05-12 14:44:42 +0000

    finished solution section

--- pages/system.py
+++ pages/system.py
@@ -90,20 +90,47 @@ def get_variable(value:str, var:str, remove_multiplier: bool = False) -> tuple[l
 def sort_variable(var:list[str, list[int]]) -> tuple[int, str]:
     return (-1 if var[1][0] >= 0 else 1, var[0])
 
+def get_value(i: int, v: list[str, list[int, int]], m:int = 1) -> str:
+    sign = ''
+    if v[1][0] >= 0:
+        sign += '+' if i > 0 else ''
+    else:
+        sign += '-'
+    body = f"{abs(v[1][0]*(m//v[1][1]))}{v[0]}"
+    return sign + body
+
 def write_equation(equation: list, multiplier:str, first_sign: bool = False):
     res = ""
     if len(equation) == 0:
         return res
-    common = lcm(*[v[1][1] for v in equation])
-    all_negative = all(v[1][1] < 0 for v in equation)
+    all_negative = all(v[1][0] < 0 for v in equation)
     if all_negative:
         equation = [[v[0], [-v[1][0], v[1][1]]] for v in equation]
-    if common == 1 and len(equation) == 1:
-        v = equation[0]
-        sign = "-" if all_negative else ("+" if first_sign else "")
-        body = f"{v[1][0]}{multiplier.replace('1', '')}{v[0]}"
-        res = sign + body
-    st.write(res)
+    common = lcm(*[v[1][1] for v in equation])
+    sign = "-" if all_negative else ("+" if first_sign else "")
+    if common == 1:
+        if len(equation) == 1:
+            v = equation[0]
+            body = f"{v[1][0]}{multiplier.replace('1', '')}{v[0]}"
+            res = sign + body
+        else:
+            right = ''.join(get_value(i, v) for i, v in enumerate(sorted(equation, key=sort_variable)))
+            body = f"{multiplier.replace('1', '')}({right})"
+            res = sign + body
+    else:
+        if len(equation) == 1:
+            v = equation[0]
+            uper = f"{v[1][0]}{multiplier.replace('1', '')}{v[0]}"
+            lower = f"{v[1][1]}"
+            body = f"\\frac{{{uper}}}{{{lower}}}"
+            res = sign + body
+        else:
+            uper = f"{multiplier}"
+            lower = f"{common}"
+            right = ''.join(get_value(i, v, common) for i, v in enumerate(sorted(equation, key=sort_variable)))
+            body = f"\\frac{{{uper}}}{{{lower}}}({right})"
+            res = sign + body
+    return res
 
 def write_system_solution(matrix, num_stats:int, solution_introduction:str = "") -> str:
     total = len(matrix[0])
@@ -124,11 +151,14 @@ def write_system_solution(matrix, num_stats:int, solution_introduction:str = "")
             if free[1][0] != 0:
                 equation[kind].append(free)
         latex_code += depedent[0] + "="
-        write_equation(equation["pure"], "p")
-        write_equation(equation["mixed"], "m", True)
-        write_equation(equation["stats"], "1", True)
-        latex_code += "\\\\\n"
-    latex_code += "$$"
+        latex_code += write_equation(equation["pure"], "p")
+        latex_code += write_equation(equation["mixed"], "m", True)
+        latex_code += write_equation(equation["stats"], "1", True)
+        latex_code += ("\\\\" if i < num_stats - 1 else "") + "\n"
+    latex_code += "\\end{cases}$$"
+    markdown += latex_code
+    st.code(markdown, language="latex")
+    return latex_code
 
 st.title("System")
 
@@ -147,8 +177,10 @@ with st.expander("Sobre"):
 
 num_stats = st.number_input("Number of Stats", min_value=2, max_value=10, step=1, value=5, help="Número de atributos (stats) no sistema.")
 extended_matrix_introduction = st.text_input("Extended Matrix Introduction", value="the extended matrix is given by:", help="texto de apresentação da matriz estendida.")
-extended_matrix_visualization = st.checkbox("visualizar resultado final?")
+extended_matrix_visualization = st.checkbox("visualizar resultado final?",key=0)
 extended_sage_code_introduction = st.text_input("Sage Code Introduction", value="the sage code is given by:", help="texto de apresentação do codigo sage.")
+solution_introduction = st.text_input("solution inntroduction", value="the solution is given by:")
+solution_visualization = st.checkbox("visualizar resultado final?",key=1)
 if st.button("gerar codigo"):
     extended_matrix = generate_extended_matrix(num_stats)
     with st.expander("Extended Matrix"):
@@ -160,5 +192,10 @@ if st.button("gerar codigo"):
     with st.expander("Sage Code"):
         reduced_matrix = write_sage_steps(extended_matrix, num_stats, extended_sage_code_introduction, False)
     with st.expander("System Solution"):
-        write_system_solution(reduced_matrix, num_stats)
+        code = write_system_solution(reduced_matrix, num_stats, solution_introduction)
+        if solution_visualization:
+            code = code.replace("$$\\begin{cases}", "$$\n\\left\\{\\begin{array}{cc}")
+            code = code.replace("\\end{cases}$$", "\\end{array}\\right.\n$$")
+            code = code.replace("=", "=&")
+            st.write(code)
     
